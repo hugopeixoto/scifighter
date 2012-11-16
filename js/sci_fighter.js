6c15bda9aa85cb8de8a85955c56612723b753f1a
Hugo Peixoto <hugo.peixoto@ndrive.com>
2012-11-16 03:07:36 +0000

    Programações movimentativas

--- js/sci_fighter.js
+++ js/sci_fighter.js
@@ -1,13 +1,16 @@
 
 function Cell () {
     this.type = undefined;
-    this.walkable = false;
+    this.walkable = function () {
+        return [ Cell.types.GROUND_LAVA, Cell.types.GROUND_SWAMP, Cell.types.GROUND_STONE ].indexOf(this.type) >= 0;
+    }
     this.objects = [];
 }
 
 function Player () {
     this.x = 16;
     this.y = 31;
+    this.orientation = Player.orientations.NORTH;
 }
 
 function Level () {
@@ -25,20 +28,53 @@ function Level () {
             }
         }
     }
+
+    this.withinBounds = function (x, y) {
+        return x >= 0 && y >= 0 && x < this.width && y < this.height;
+    }
 }
 
 function SciFighter () {
     this.level = new Level();
+    this.last_action = 150;
+
     this.update = function (modifier, action) {
+        this.level.player.orientation = action;
+
+        var x = this.level.player.x;
+        var y = this.level.player.y;
+        switch (action) {
+            case SciFighter.actions.LEFT:
+              x--;
+              break;
+            case SciFighter.actions.UP:
+              y--;
+              break;
+            case SciFighter.actions.RIGHT:
+              x++;
+              break;
+            case SciFighter.actions.DOWN:
+              y++;
+              break;
+        }
+
+        this.last_action -= modifier * 1000;
+        if (this.last_action < 0 && this.level.withinBounds(x, y) && this.level.grid[y][x].walkable()) {
+            this.level.player.x = x;
+            this.level.player.y = y;
+            this.last_action = 150;
+        }
     }
 }
 
-SciFighter.actions = {
+Player.orientations = {
   LEFT: 1,
   UP: 2,
   RIGHT: 3,
   DOWN: 4
 }
+
+SciFighter.actions = Player.orientations;
 
 Cell.types = {
     LAVA: 1,
