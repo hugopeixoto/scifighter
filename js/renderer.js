15bb03d5d8afc53f796b031808acea7a821fba96
Hugo Peixoto <hugo.peixoto@ndrive.com>
2012-11-16 00:38:48 +0000

    Fixes the poops

--- js/renderer.js
+++ js/renderer.js
@@ -3,26 +3,26 @@ var Renderer = function(canvas, ctx)
 	this.ctx = ctx;
 	this.canvas = canvas;
 
-	this.render = function (board, ctx) {
+	this.render = function (board) {
 		/*if (bgReady) {
 			ctx.drawImage(bgImage, 0, 0);
 		}*/
 
 
 
-		ctx.clearRect ( 0 , 0 , canvas.width , canvas.height );
+		this.ctx.clearRect( 0 , 0 , canvas.width , canvas.height );
 
-		drawBoard(board, ctx);
+		this.drawBoard(board);
 
 		// Test text
-		ctx.fillStyle = "rgb(250, 250, 250)";
-		ctx.font = "24px Helvetica";
-		ctx.textAlign = "left";
-		ctx.textBaseline = "top";
-		ctx.fillText("I HAZ ZE TEXT!!!1!!!");
+		this.ctx.fillStyle = "rgb(250, 250, 250)";
+		this.ctx.font = "24px Helvetica";
+		this.ctx.textAlign = "left";
+		this.ctx.textBaseline = "top";
+		this.ctx.fillText("I HAZ ZE TEXT!!!1!!!", canvas.width/2-100, canvas.width/2-50);
 	};
 
-	this.drawBoard = function (board, ctx)
+	this.drawBoard = function (board)
 	{
 
 	}
