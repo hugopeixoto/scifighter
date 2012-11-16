8e90a3bb77b49ed9d7b5738688ded24bd7f1027c
Fabio Aguiar <fabio.mil3s@gmail.com>
2012-11-16 18:51:49 +0000

    added dummie battle screen

--- js/renderer.js
+++ js/renderer.js
@@ -53,23 +53,31 @@ var Renderer = function(canvas, ctx)
     this.spriteBatch["player"][Player.orientations.RIGHT] = new Sprite("joao/right.png");
 
 	this.render = function (scifighter) {
-		/*if (bgReady) {
-			ctx.drawImage(bgImage, 0, 0);
-		}*/
-
 
 
 		this.ctx.clearRect( 0 , 0 , canvas.width , canvas.height );
 
-		this.drawLevel(scifighter);
+		if(scifighter.state == 0)
+		{
+			this.drawLevel(scifighter);
+		}
+		else if(scifighter.state == 1)
+		{
+			this.drawBattle(scifighter);
+		}
 
-		// Test text
-		//this.ctx.fillStyle = "rgb(250, 250, 250)";
-		//this.ctx.font = "24px Helvetica";
-		//this.ctx.textAlign = "left";
-		//this.ctx.textBaseline = "top";
-		//this.ctx.fillText("I HAZ ZE TEXT!!!1!!!", canvas.width/2-100, canvas.width/2-50);
+		
 	};
+
+	this.drawBattle = function(scifighter)
+	{
+		// Test text
+		this.ctx.fillStyle = "rgb(250, 250, 250)";
+		this.ctx.font = "24px Helvetica";
+		this.ctx.textAlign = "left";
+		this.ctx.textBaseline = "top";
+		this.ctx.fillText("battle!", canvas.width/2-100, canvas.width/2-50);
+	}
 
 	this.drawLevel = function (scifighter)
 	{
